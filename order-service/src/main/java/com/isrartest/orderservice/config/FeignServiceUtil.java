bfbaaf2f19a7173c02054d1b1d325f86d9ed40b8
i-israr <israrulhaq736@gmail.com>
2023-08-18 17:07:16 +0500

    Server disconvery server has been done

--- order-service/src/main/java/com/isrartest/orderservice/config/FeignServiceUtil.java
+++ order-service/src/main/java/com/isrartest/orderservice/config/FeignServiceUtil.java
@@ -1,19 +1,15 @@
 package com.isrartest.orderservice.config;
 
 import com.isrartest.orderservice.dto.InventoryResponse;
-import org.hibernate.query.QueryParameter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.management.Query;
 import java.util.List;
 
-@FeignClient(value = "feignClient", url = "http://localhost:8082/api/inventory")
+@FeignClient(value = "inventory-service")
 public interface FeignServiceUtil {
 
-    @GetMapping
+    @GetMapping("/api/inventory")
     InventoryResponse[] isInStock(@RequestParam("skuCode") List<String> skuCode);
 
 }
